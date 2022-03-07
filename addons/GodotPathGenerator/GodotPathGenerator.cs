6abbb86b87ac9616fc7eafef6751b10ba6e09b36
LiXin-Link <1191792984@qq.com>
2022-03-07 14:38:39 +0800

    【更新】更新至0.3版本，添加了 删除、移动、重命名脚本文件时自动修改对应的Path类

--- addons/GodotPathGenerator/GodotPathGenerator.cs
+++ addons/GodotPathGenerator/GodotPathGenerator.cs
@@ -1,5 +1,9 @@
+#define DEBUG
+
+using System;
 using Godot;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using static Godot.GD;
 
@@ -9,92 +13,136 @@ public class GodotPathGenerator : EditorPlugin
     private const string PluginName = "GodotPathGenerator";
 
     private const string ClassNameRegex = "(?<=[//|\\.])(@*[a-zA-Z0-9]+)(?=\\.cs$)";
-    
-    private string _dirPath = "res://script/gpg";
+
+    private const string DirPath = "res://script/gpg";
 
     private EditorInterface _editor;
+
+    /// <summary>
+    /// key: script path
+    /// value: class name
+    /// </summary>
+    private readonly Dictionary<string, string> _generatedFilePath = new Dictionary<string, string>();
+
+    private readonly Directory _dir = new Directory();
+
+    private (string, DateTime) _lastChange = ("", DateTime.Now);
 
     public override void _EnterTree()
     {
         _editor = GetEditorInterface();
         EditorFileSystem fileSystem = _editor.GetResourceFilesystem();
+        FileSystemDock fileSystemDock = _editor.GetFileSystemDock();
 
         fileSystem.Connect("filesystem_changed", this, nameof(OnFilesystemChanged));
+        
+        fileSystemDock.Connect("files_moved", this, nameof(OnFilesMoved));
+        fileSystemDock.Connect("file_removed", this, nameof(OnFileRemoved));
+        fileSystemDock.Connect("folder_removed", this, nameof(OnFolderRemoved));
 
         Print($"{PluginName}: Start Plugin");
     }
-
+    
     private void OnFilesystemChanged()
     {
+        CreateEditingSceneNodePath();
+        
+#if DEBUG
+        Print("OnFilesystemChanged");
+#endif
+    }
+    
+    #region create node path
+
+    /// <summary>
+    /// 创建当前正在编辑的场景的路径
+    /// </summary>
+    private void CreateEditingSceneNodePath()
+    {
         Node root = _editor.GetEditedSceneRoot();
-        if (root != null)
+        if (root == null) return;
+        
+        if (root.Name == _lastChange.Item1 && (DateTime.Now - _lastChange.Item2).Milliseconds < 100)
         {
-            var reference = root.GetScript();
+            return;
+        }
+            
+        var reference = root.GetScript();
 
-            if (reference != null && reference is CSharpScript script)
+        if (reference is CSharpScript script && _dir.FileExists(script.ResourcePath))
+        {
+            var resourcePath = script.ResourcePath;
+            var matchCollection = Regex.Matches(resourcePath, ClassNameRegex);
+
+            if (matchCollection.Count > 0)
             {
-                var resourcePath = script.ResourcePath;
-                var matchCollection = Regex.Matches(resourcePath, ClassNameRegex);
-
-                if (matchCollection.Count > 0)
+                var nameMatch = matchCollection[0];
+                var pathList = new List<string>();
+                    
+                TraversalChildren("", root, pathList);
+                var generated = StartGeneratePath(nameMatch.ToString(), pathList);
+                    
+                if (generated)
                 {
-                    var pathList = new List<string>();
-                    TraversalChildren("", root, pathList);
-                    var nameMatch = matchCollection[0];
-
-                    StartGeneratePath(nameMatch.ToString(), pathList);
+                    if (_generatedFilePath.ContainsKey(resourcePath))
+                    {
+                        _generatedFilePath[resourcePath] = nameMatch.ToString();
+                    }
+                    else
+                    {
+                        _generatedFilePath.Add(resourcePath, nameMatch.ToString());
+                    }
+                    Print($"{PluginName}: generated node path");
                 }
             }
         }
 
-        Print("OnFilesystemChanged");
+        _lastChange.Item1 = root.Name;
+        _lastChange.Item2 = DateTime.Now;
     }
 
     private void TraversalChildren(string prefix, Node start, List<string> pathList)
     {
         var path = prefix + "/" + start.Name;
         pathList.Add(path);
-        Print(path);
 
         var children = start.GetChildren();
-        if (children != null && children.Count > 0)
+        if (children == null || children.Count <= 0) return;
+        
+        foreach (var child in children)
         {
-            foreach (var child in children)
+            if (child is Node node)
             {
-                if (child is Node node)
-                {
-                    TraversalChildren(path, node, pathList);
-                }
+                TraversalChildren(path, node, pathList);
             }
         }
     }
 
-    private void StartGeneratePath(string className, List<string> pathList)
+    private bool StartGeneratePath(string className, List<string> pathList)
     {
         string fileName = $"{className}Path.cs";
-        string filePath = _dirPath + "/" + fileName;
+        string filePath = DirPath + "/" + fileName;
 
-        var scriptDir = new Directory();
         var file = new File();
         try
         {
-            if (!scriptDir.DirExists(_dirPath))
+            if (!_dir.DirExists(DirPath))
             {
-                if (scriptDir.MakeDirRecursive(_dirPath) != Error.Ok)
+                if (_dir.MakeDirRecursive(DirPath) != Error.Ok)
                 {
-                    PrintErr($"{PluginName}: can't create '{_dirPath}' dir");
-                    return;
+                    PrintErr($"{PluginName}: can't create '{DirPath}' dir");
+                    return false;
                 }
             }
 
-            if (scriptDir.Open(_dirPath) == Error.Ok)
+            if (_dir.Open(DirPath) == Error.Ok)
             {
                 if (file.Open(filePath, File.ModeFlags.Write) != Error.Ok)
                 {
                     PrintErr($"{PluginName}: can't create file '{filePath}'");
-                    return;
+                    return false;
                 }
-                
+
                 file.StoreString("/// <summary>");
                 file.StoreString(System.Environment.NewLine);
                 file.StoreString("/// Don't modify this file, let plugin update it");
@@ -114,10 +162,12 @@ public class GodotPathGenerator : EditorPlugin
                 file.Flush();
 
                 file.Close();
+                return true;
             }
             else
             {
-                PrintErr($"{PluginName}: can't open '{_dirPath}' dir");
+                PrintErr($"{PluginName}: can't open '{DirPath}' dir");
+                return false;
             }
         }
         finally
@@ -147,4 +197,80 @@ public class GodotPathGenerator : EditorPlugin
         file.StoreString("    }");
         file.StoreString(System.Environment.NewLine);
     }
+
+    #endregion
+
+    public void OnFileRemoved(string filePath)
+    {
+        if (_generatedFilePath.ContainsKey(filePath))
+        {
+            var className = _generatedFilePath[filePath];
+            RemovePathFileByClassName(className);
+
+            _generatedFilePath.Remove(filePath);
+        }
+#if DEBUG
+        Print($"OnFileRemoved; filePath: {filePath}");
+#endif
+    }
+
+    public void OnFilesMoved(string oldFile, string newFile)
+    {
+        if (_generatedFilePath.ContainsKey(oldFile))
+        {
+            var oldClassNameMatchCollection = Regex.Matches(oldFile, ClassNameRegex);
+            var newClassNameMatchCollection = Regex.Matches(newFile, ClassNameRegex);
+
+            if (oldClassNameMatchCollection.Count > 0 && newClassNameMatchCollection.Count > 0)
+            {
+                var oldClassNameMatch = oldClassNameMatchCollection[0];
+                var newClassNameMatch = newClassNameMatchCollection[0];
+                if (oldClassNameMatch == newClassNameMatch)
+                {
+                    return;
+                }
+                
+                string oldPath = $"{DirPath}/{oldClassNameMatch}Path.cs";
+                string newPath = $"{DirPath}/{newClassNameMatch}Path.cs";
+                
+                _dir.Rename(oldPath, newPath);
+            }
+        }
+        
+#if DEBUG
+        Print($"OnFilesMoved; oldFile: {oldFile}, newFile: {newFile}");
+#endif
+    }
+
+    public void OnFolderRemoved(string folder)
+    {
+        var classNameList = _generatedFilePath.Keys
+            .Where(path => path.StartsWith(folder))
+            .ToList();
+        foreach (var scriptPath in classNameList)
+        {
+            RemovePathFileByClassName(_generatedFilePath[scriptPath]);
+            _generatedFilePath.Remove(scriptPath);
+        }
+        
+#if DEBUG
+        Print($"OnFolderRemoved; folder: {folder}");
+#endif
+    }
+
+    private void RemovePathFileByClassName(string className)
+    {
+        string classPath = $"{DirPath}/{className}Path.cs";
+
+        if (_dir.FileExists(classPath))
+        {
+            if (_dir.Remove(classPath) == Error.Ok)
+            {
+#if DEBUG
+                Print($"{PluginName}: remove a node path file");
+#endif
+            }
+        }
+    }
+    
 }
